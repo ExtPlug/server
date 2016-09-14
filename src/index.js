0c0992bb2170d15c89d716f2b04f7b83f6e4b993
René Kooi <renee@kooi.me>
2016-09-14 18:57:37 +0200

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-const express = require('express')
-const request = require('request')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const PORT = process.env.EXTP_PORT || 34569
+const PORT = process.env.EXTP_PORT || 34569;
 
-const app = express()
+const app = express();
 
-app.use(cors({ origin: 'https://plug.dj' }))
-app.use(require('body-parser').urlencoded())
-app.use(require('body-parser').json())
-app.use(require('express-session')({ secret: 'secret' }))
+app.use(cors({ origin: 'https://plug.dj' }));
+app.use(require('body-parser').urlencoded());
+app.use(require('body-parser').json());
+app.use(require('express-session')({ secret: 'secret' }));
 
-app.use('/rs', require('./room-settings-proxy'))
+app.use('/rs', require('./room-settings-proxy'));
 
 app.listen(PORT, () => {
-  console.info(`listening on ${PORT}`)
-})
+  console.info(`listening on ${PORT}`);
+});
--- src/room-settings-proxy.js
+++ src/room-settings-proxy.js
@@ -1,20 +1,22 @@
-const request = require('request')
+const request = require('request');
 
 // _SUPER_ barebones proxy for room settings json
 export default function (req, res) {
-
   // chop off the leading slash
-  let url = req.url.substr(1)
+  const url = req.url.substr(1);
 
-  request.get({ url: url
-                // we're fine with misconfigured servers
-              , rejectUnauthorized: false
-              , headers:
-                { 'user-agent': 'ExtPlug Room Settings Proxy' } })
-    .on('response', res => {
+  request.get({
+    url,
+    // we're fine with misconfigured servers
+    rejectUnauthorized: false,
+    headers: {
+      'user-agent': 'ExtPlug Room Settings Proxy',
+    },
+  })
+    .on('response', (targetRes) => {
       // some room settings don't set their content types properly
-      res.headers['content-type'] = 'application/json'
+      // eslint-disable-next-line no-param-reassign
+      targetRes.headers['content-type'] = 'application/json';
     })
-    .pipe(res)
-
+    .pipe(res);
 }
