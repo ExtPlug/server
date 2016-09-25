b9be68ad534e0314c5361d75a4a1704184f516e9
René Kooi <renee@kooi.me>
2016-09-25 19:03:40 +0200

    remove babel & target node 4

--- src/room-settings-proxy.js
+++ src/room-settings-proxy.js
@@ -1,22 +1,17 @@
 const request = require('request');
 
 // _SUPER_ barebones proxy for room settings json
-export default function (req, res) {
+module.exports = (req, res) => {
   // chop off the leading slash
   const url = req.url.substr(1);
 
-  request.get({
-    url,
+  res.set('content-type', 'application/json');
+
+  request(url, {
     // we're fine with misconfigured servers
     rejectUnauthorized: false,
     headers: {
       'user-agent': 'ExtPlug Room Settings Proxy',
     },
-  })
-    .on('response', (targetRes) => {
-      // some room settings don't set their content types properly
-      // eslint-disable-next-line no-param-reassign
-      targetRes.headers['content-type'] = 'application/json';
-    })
-    .pipe(res);
-}
+  }).pipe(res);
+};
