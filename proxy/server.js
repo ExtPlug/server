54c144110477a5cc80a8d1a1b91f5f4c8bb7eaa7
Renée Kooi <renee@kooi.me>
2017-07-15 11:25:49 +0200

    proxy: Do not require `origin` header

--- proxy/server.js
+++ proxy/server.js
@@ -1,1 +1,8 @@
-require('cors-anywhere/server');
+const cors = require('cors-anywhere')
+
+cors.createServer({
+  removeHeaders: [
+    'cookie',
+    'cookie2'
+  ]
+}).listen(process.env.PORT || 8080)
